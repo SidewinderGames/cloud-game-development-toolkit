6fb8be1a242d8ced783ebf3379fbc3b7b6afc69d
looter <looterz@users.noreply.github.com>
2026-05-16 14:11:16 -0400

    update vars

--- studio/phase2-horde/variables.tf
+++ studio/phase2-horde/variables.tf
@@ -57,9 +57,9 @@ variable "horde_host_instance_type" {
 }
 
 variable "horde_image_tag" {
-  description = "Tag of the Horde Server image in the Sidewinder ECR repository. Bump after pushing a new build (e.g. \"5.7.4-datavol1\")."
+  description = "Tag of the Horde Server image in the Sidewinder ECR repository. Bump after pushing a new build (e.g. \"5.7.4-datavol2\")."
   type        = string
-  default     = "5.7.4-datavol1"
+  default     = "5.7.4-datavol2"
 }
 
 variable "horde_data_volume_size" {
